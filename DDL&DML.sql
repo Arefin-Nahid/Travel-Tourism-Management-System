322c08ca04909ba9f984e5c8710dafd085d03a5e
Arefin Nahid <134856826+Arefin-Nahid@users.noreply.github.com>
2024-05-06 21:40:06 +0600

    Update DDL&DML.sql

--- DDL&DML.sql
+++ DDL&DML.sql
@@ -1,24 +1,24 @@
 --1. Customer Table
 
 CREATE TABLE Customer ( customerID NUMBER PRIMARY KEY, customer_name VARCHAR2(100), customer_NID VARCHAR2(20), passport VARCHAR2(20), address VARCHAR2(200), phone VARCHAR2(15), email VARCHAR2(100) );
- -- Sample Data Insertion 
 
+-- Sample Data Insertion 
 INSERT INTO Customer VALUES (1, 'Miraz', '1234567890', 'AB1234567', '123 Dhaka', '0123456789', 'miraz@gmail.com'); 
 INSERT INTO Customer VALUES (2, 'Smith', '0987654321', 'CD7654321', '456 khulna', '9876543210', 'smith@gmail.com'); 
 
 --2. Booking Table
 
 CREATE TABLE Booking ( bookingID NUMBER PRIMARY KEY, customerID NUMBER, date DATE, customer_name VARCHAR2(100), package_cost NUMBER, add_pay NUMBER, FOREIGN KEY (customerID) REFERENCES Customer(customerID) ); 
- -- Sample Data Insertion
-
-INSERT INTO Booking VALUES (1, 1, TO_DATE('2024-05-20', 'YYYY-MM-DD'), `Miraz`, 1500, 300); INSERT INTO Booking VALUES (2, 2, TO_DATE('2024-05-22', 'YYYY-MM-DD'), `Amit`, 1800, 200); 
+ 
+-- Sample Data Insertion
+INSERT INTO Booking VALUES (1, 1, TO_DATE('2024-05-20', 'YYYY-MM-DD'), `Miraz`, 1500, 300);
+INSERT INTO Booking VALUES (2, 2, TO_DATE('2024-05-22', 'YYYY-MM-DD'), `Amit`, 1800, 200); 
 
 --3. Package Table
 
 CREATE TABLE Package ( packageID NUMBER PRIMARY KEY, Tourtime VARCHAR2(50), location VARCHAR2(100), start_location VARCHAR2(100), end_location VARCHAR2(100), capacity NUMBER, remain_capacity NUMBER, duration NUMBER, food_type VARCHAR2(50), price NUMBER );
 
  -- Sample Data Insertion 
-
 INSERT INTO Package VALUES (1, 'Morning', 'Sundarban', 'City A', 'City B', 40, 10, 3, 'Vegetarian', 1500); 
 INSERT INTO Package VALUES (2, 'Evening', 'Coxs Bazar', 'City C', 'City D', 30, 5, 2, 'Non-Vegetarian', 1800); 
 
